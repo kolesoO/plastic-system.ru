81093004c0e5e32c24317ee1960a1d8229096e5f
Aleksey Kolesnichenko <alekseykolesnichenko@MacBook-Air-Aleksey.local>
2019-09-05 23:45:09 +0300

    add jivosite

--- local/templates/common/footer.php
+++ local/templates/common/footer.php
@@ -209,5 +209,9 @@
     }
     //end
     ?>
+    <script type='text/javascript'>
+        (function(){ var widget_id = '107881';
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();
+    </script>
 </body>
 </html>
