4df37eeb5c43be6ae054b0bbba4f3e53c9d87e6e
Plsm dev server <noreply@plsm.ru>
2019-09-09 00:09:35 +0300

    last changes on prod

--- about-company/index.php
+++ about-company/index.php
@@ -7,29 +7,29 @@ $APPLICATION->SetTitle("О компании");
 ?><div class="block_wrapper big">
 	<div flex-align="center" flex-text_align="space-around">
 		<div class="col-lg-12 col-md-12 col-xs-24">
-            <div class="col-lg-21 col-md-21 colxs-24">
-                <img src="/upload/content/about-company/front.png">
-            </div>
+			<div class="col-lg-21 col-md-21 colxs-24">
+ <img src="/upload/content/about-company/front.png">
+			</div>
 			<p>
-				Выставка «РосУпак»
+				 Выставка «РосУпак»
 			</p>
 		</div>
 		<div flex-text_align="center" class="col-lg-12 col-md-12 col-xs-24">
 			<ul class="color_list">
 				<li class="color_list-item" flex-align="center">
 				<div class="color_list-number">
-					11
+					 11
 				</div>
  <span>лет на рынке <br>
-				полимерных <br>
-				материалов</span> </li>
+				 полимерных <br>
+				 материалов</span> </li>
 				<li class="color_list-item" flex-align="center">
 				<div class="color_list-number">
-					8
+					 8
 				</div>
  <span>филиалов <br>
-				на территории <br>
-				России</span> </li>
+				 на территории <br>
+				 России</span> </li>
 			</ul>
 		</div>
 	</div>
@@ -37,75 +37,94 @@ $APPLICATION->SetTitle("О компании");
 	<div flex-align="start">
 		<div class="content col-lg-12 col-md-24 col-xs-24">
 			<p>
-				Производим и поставляем многооборотную промышленную тару и упаковку. Компания была основана в 07 февраля 2008 г. в г Санкт-Петербурге.
+				 Производим и поставляем многооборотную промышленную тару и упаковку. Компания была основана в 07 февраля 2008 г. в г Санкт-Петербурге.
 			</p>
 			<p>
-				Офисы и склады находятся в г. Санкт-Петербург, Москва, Самара, Саратов, Ростов-на-Дону, Краснодар, Владивосток, Екатеринбург.
+				 Офисы и склады находятся в г. Санкт-Петербург, Москва, Самара, Саратов, Ростов-на-Дону, Краснодар, Екатеринбург, Владивосток.
 			</p>
 		</div>
 		<div class="content col-lg-12 col-md-24 col-xs-24">
 			<p>
-				Товарная линейка: ящики, паллеты, лотки, крупногабаритные контейнеры, мусорные баки, фурнитура для ящиков, стеллажи, гибкая упаковка, листовые пластики из поликарбоната, полипропилена, полимерное сырье.
+				 Товарная линейка: пластиковые ящики, паллеты, складские лотки, крупногабаритные контейнеры, мусорные баки, фурнитура для ящиков, модульные стеллажи, гибкая упаковка, листовые пластики из поликарбоната, полипропилена, полимерное сырье.
 			</p>
 			<p>
-				Разработка и создание индивидуальных решений по упаковке, таре и складской логистики. Изготовление индивидуальной упаковки от 1 шт. Поставка продукции в любой регион России, страну СНГ и Европы.
+				 Разработка и создание индивидуальных решений по упаковке, таре и складской логистики. Изготовление индивидуальной упаковки от 1 шт. Поставка продукции в любой регион России, страну СНГ и Европы.
 			</p>
 		</div>
 	</div>
 	<hr>
 	<div class="title-3">
-		Основные направления нашей деятельности
+		 Основные направления нашей деятельности
 	</div>
 	<hr>
 	<ul class="inline_list" flex-align="end">
 		<li>
-            <div class="inline_list-img">
-                <img src="/upload/content/about-company/catalog_section-1.svg" width="72" height="56">
-            </div>
-            <div><b>Поликарбонат <br>листовой</b></div>
-        </li>
+		<div class="inline_list-img">
+ <img width="72" src="/upload/content/about-company/catalog_section-1.svg" height="56">
+		</div>
+		<div>
+ <b>Поликарбонат <br>
+			 листовой</b>
+		</div>
+ </li>
 		<li>
-            <div class="inline_list-img">
-                <img src="/upload/content/about-company/catalog_section-2.svg" width="82" height="60">
-            </div>
-            <div><b>Полипропилен <br>листовой</b></div>
-        </li>
+		<div class="inline_list-img">
+ <img width="82" src="/upload/content/about-company/catalog_section-2.svg" height="60">
+		</div>
+		<div>
+ <b>Полипропилен <br>
+			 листовой</b>
+		</div>
+ </li>
 		<li>
-            <div class="inline_list-img">
-                <img src="/upload/content/about-company/catalog_section-3.svg" width="80" height="45">
-            </div>
-            <div><b>Пластиковые <br>паллеты</b></div>
-        </li>
+		<div class="inline_list-img">
+ <img width="80" src="/upload/content/about-company/catalog_section-3.svg" height="45">
+		</div>
+		<div>
+ <b>Пластиковые <br>
+			 паллеты</b>
+		</div>
+ </li>
 		<li>
-            <div class="inline_list-img">
-                <img src="/upload/content/about-company/catalog_section-4.svg" width="52" height="66">
-            </div>
-            <div><b>Модульные <br>стеллажи</b></div>
-        </li>
+		<div class="inline_list-img">
+ <img width="52" src="/upload/content/about-company/catalog_section-4.svg" height="66">
+		</div>
+		<div>
+ <b>Модульные <br>
+			 стеллажи</b>
+		</div>
+ </li>
 		<li>
-            <div class="inline_list-img">
-                <img src="/upload/content/about-company/catalog_section-5.svg" width="80" height="49">
-            </div>
-            <div><b>Лотки <br>и стеллажи</b></div>
-        </li>
+		<div class="inline_list-img">
+ <img width="80" src="/upload/content/about-company/catalog_section-5.svg" height="49">
+		</div>
+		<div>
+ <b>Складские<br>
+			 лотки</b>
+		</div>
+ </li>
 		<li>
-            <div class="inline_list-img">
-                <img src="/upload/content/about-company/catalog_section-6.svg" width="73" height="53">
-            </div>
-            <div><b>Пластиковые <br>контейнеры</b></div>
-        </li>
+		<div class="inline_list-img">
+ <img width="73" src="/upload/content/about-company/catalog_section-6.svg" height="53">
+		</div>
+		<div>
+ <b>Пластиковые <br>
+			 контейнеры</b>
+		</div>
+ </li>
 	</ul>
 	<hr>
 	<div flex-align="start">
 		<div class="content col-lg-12">
 			<p>
-				Вы будете довольны сотрудничеством с нашей компанией, ведь мы предоставляем качественную продукцию по минимальной цене. Для оптовых покупателей активна система скидок, а те, кто покупает любой товар в розницу, (к примеру, складские лотки) оценят демократичную его стоимость.
+				 Вы будете довольны сотрудничеством с нашей компанией, ведь мы не только предоставляем качественную продукцию по минимальной цене, а имеем комплексный подход к каждому клиенту, начиная от помощи в подборе тары или упаковки в зависимости от условий эксплуатации, возможности нанесения логотипа компании, а также реализацию нестандартных проектов.
 			</p>
 		</div>
 		<div class="content col-lg-12">
 			<p>
-				Вы можете отправить заявку на покупку любого из представленных продуктов в нашем интернет-магазине или посетит нас лично (СПб, Московский пр., д.93, МСК Ленинградское ш. 69). Также действуют заявки по телефону <b>(812)313-75-50</b>.
+				 Вы можете отправить заявку на покупку любого из представленных продуктов в нашем сайте&nbsp;или обратиться за консультацией в отдел продаж по телефону <b>+7</b><b>(812)313-75-50</b>.
 			</p>
 		</div>
 	</div>
-</div><?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
+</div>
+ <br><?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
