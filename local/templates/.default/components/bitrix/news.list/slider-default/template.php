204713dec12cecfe7e0d00c6d1518d4379851732
Aleksey Kolesnichenko <alekseykolesnichenko@MacBook-Air-Aleksey.local>
2019-10-31 19:13:12 +0300

    update default slider

--- local/templates/.default/components/bitrix/news.list/slider-default/template.php
+++ local/templates/.default/components/bitrix/news.list/slider-default/template.php
@@ -1,32 +1,38 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
 
 <?if ($arResult["ITEMS_COUNT"] > 0) :?>
-    <section
-        class="default_slider clearfix js-slider"
-        data-autoplay="true"
-        data-autoplaySpeed="5000"
-        data-infinite="true"
-        data-speed="1000"
-        data-arrows="false"
-        data-dots="true"
-    >
-        <?foreach ($arResult["ITEMS"] as $arItem) :
-            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-            ?>
-            <?if (strlen($arItem["PROPERTIES"]["LINK"]["VALUE"]) > 0) :?>
-                <a id="<?=$this->GetEditAreaId($arItem['ID']);?>" href="<?=$arItem["PROPERTIES"]["LINK"]["VALUE"]?>" class="default_slider-item" style="background-image:url('<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>')">
-                    <div class="container">
-                        <div class="default_slider-desc"><?=($arItem["PREVIEW_TEXT_TYPE"] == "text" ? $arItem["PREVIEW_TEXT"] : htmlspecialcharsback($arItem["PREVIEW_TEXT"]))?></div>
+    <section class="section">
+        <?if (isset($arResult["SECTION"]["PATH"][0])) :?>
+            <div class="title-2"><?=$arResult["SECTION"]["PATH"][0]["NAME"]?></div>
+        <?endif?>
+        <div
+                class="default_slider clearfix js-slider"
+                data-autoplay="true"
+                data-autoplaySpeed="3000"
+                data-infinite="true"
+                data-fade="true"
+                data-speed="1000"
+                data-arrows="false"
+                data-dots="true"
+        >
+            <?foreach ($arResult["ITEMS"] as $arItem) :
+                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                ?>
+                <?if (strlen($arItem["PROPERTIES"]["LINK"]["VALUE"]) > 0) :?>
+                    <a id="<?=$this->GetEditAreaId($arItem['ID']);?>" href="<?=$arItem["PROPERTIES"]["LINK"]["VALUE"]?>" class="default_slider-item" style="background-image:url('<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>')">
+                        <div class="container">
+                            <div class="default_slider-desc"><?=($arItem["PREVIEW_TEXT_TYPE"] == "text" ? $arItem["PREVIEW_TEXT"] : htmlspecialcharsback($arItem["PREVIEW_TEXT"]))?></div>
+                        </div>
+                    </a>
+                <?else:?>
+                    <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="default_slider-item" style="background-image:url('<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>')">
+                        <div class="container">
+                            <div class="default_slider-desc"><?=($arItem["PREVIEW_TEXT_TYPE"] == "text" ? $arItem["PREVIEW_TEXT"] : htmlspecialcharsback($arItem["PREVIEW_TEXT"]))?></div>
+                        </div>
                     </div>
-                </a>
-            <?else:?>
-                <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="default_slider-item" style="background-image:url('<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>')">
-                    <div class="container">
-                        <div class="default_slider-desc"><?=($arItem["PREVIEW_TEXT_TYPE"] == "text" ? $arItem["PREVIEW_TEXT"] : htmlspecialcharsback($arItem["PREVIEW_TEXT"]))?></div>
-                    </div>
-                </div>
-            <?endif?>
-        <?endforeach;?>
+                <?endif?>
+            <?endforeach;?>
+        </div>
     </section>
 <?endif?>
