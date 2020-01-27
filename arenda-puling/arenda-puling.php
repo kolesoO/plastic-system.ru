ca1bf17554ee3ccc696ba2123197f7ac514b1cad
Plsm dev server <noreply@plsm.ru>
2020-01-27 23:38:01 +0300

    last changes on prod

--- arenda-puling/arenda-puling.php
+++ arenda-puling/arenda-puling.php
@@ -4,111 +4,212 @@ $APPLICATION->SetPageProperty("description", "Аренда (пулинг) скл
 $APPLICATION->SetPageProperty("title", "Информация об аренде пластиковой тары");
 $APPLICATION->SetPageProperty("header_section-class", "section");
 $APPLICATION->SetTitle("Аренда(пулинг)");
-?><div class="block_wrapper big">
+?>
+		<div class="block_wrapper big">
 			<div flex-align="center" flex-text_align="space-around">
-				<div class="col-lg-12 col-md-12 col-xs-24">
+				<div class="col-lg-12 col-md-12 col-xs-24" align="center">
 					<div class="col-lg-21 col-md-21 colxs-24">
-		 <img src="/upload/content/about-company/schema.png">
+		 <img src="https://plastic-system.ru/arenda-puling/pullinghard.png">
 					</div>
-					<p>
+					<h3>
 						 Аренда с обслуживанием
-					</p>
+					</h3>
 				</div>
-				<div class="col-lg-12 col-md-12 col-xs-24">
+				<div class="col-lg-12 col-md-12 col-xs-24" align="center">
 					<div class="col-lg-21 col-md-21 colxs-24">
-		 <img src="/upload/content/about-company/schema2.png">
+		 <img src="https://plastic-system.ru/arenda-puling/pulling.jpg">
 					</div>
-					<p>
+					<h3>
 						 Простая Аренда
-					</p>
+					</h3>
 				</div>
 			</div>
 			<hr>
 			<div flex-align="start">
-				<div class="content col-lg-12 col-md-24 col-xs-24">
+				<div class="content col-lg-12 col-md-24 col-xs-24" align="center">
 					<p>
-						 Поставщик арендует тару у Пулера на один круг по фиксированной цене и передает ее с продукцией Ритейлеру. Ритейлер, после реализации товара, возвращает тару Пулеру. Пулер проводит санитарную обработку, необходимый ремонт и замену непригодной к использованию тары и передает ее Поставщику для следующего круга.
+						 Поставщик арендует тару у Пулера на один круг по фиксированной цене и передает ее с продукцией Ритейлеру.
+					</p>
+					<p>
+						Ритейлер, после реализации товара, возвращает тару Пулеру. Пулер проводит санитарную обработку, необходимый ремонт и замену непригодной к использованию тары и передает ее Поставщику для следующего круга.
 					</p>
 				</div>
-				<div class="content col-lg-12 col-md-24 col-xs-24">
+				<div class="content col-lg-12 col-md-24 col-xs-24" align="center">
 					<p>
 						 Пулер предоставляет Арендатору тару в долгосрочное пользование. На усмотрение Арендатора Пулер может предоставлять дополнительные услуги: мойка, восстановление/ремонта и хранение тары.
 					</p>
 				</div>
 			</div>
 			<hr>
-			<div class="title-3">
-				 Основные направления нашей деятельности
+	<div align="center" class="title-3">
+		 Основные виды пластиковой тары для аренды
+	</div>
+	<hr>
+	<ul class="inline_list" flex-align="center" flex-text_align="space-around">
+		<li>
+		<div class="inline_list-img">
+ <img width="80" src="https://plastic-system.ru/upload/resize_cache/iblock/efd/62_45_1/efd9b0b28f68b81a5488867d418a2c87.png" height="49">
+		</div>
+		<div align="center">
+ <b>Крупногабаритные <br>
+			 контейнеры</b>
+		</div>
+ </li>
+		<li>
+		<div class="inline_list-img">
+ <img width="80" src="https://plastic-system.ru/upload/resize_cache/iblock/d50/62_45_1/d503bce63fe731fed61f253e81c984a9.png" height="45">
+		</div>
+		<div align="center">
+ <b>Пластиковые <br>
+			 паллеты</b>
+		</div>
+ </li>
+		<li>
+		<div class="inline_list-img">
+ <img width="80" src="https://plastic-system.ru/upload/resize_cache/iblock/a39/62_45_1/a39fe74c68d9bbfcde76e839413b67d9.png" height="49">
+		</div>
+		<div align="center">
+ <b>Складские<br>
+			 лотки</b>
+		</div>
+ </li>
+		<li>
+		<div class="inline_list-img">
+ <img width="80" src="https://plastic-system.ru/upload/resize_cache/iblock/4a1/62_45_1/4a15e91cbe34b68ee700a36f76795253.png" height="53">
+		</div>
+		<div align="center">
+ <b>Пластиковые <br>
+			 контейнеры</b>
+		</div>
+ </li>
+		<li>
+		<div class="inline_list-img">
+ <img width="80" src="https://plastic-system.ru/upload/resize_cache/iblock/790/62_45_1/7909479f7892991f215a2755eb3af369.png" height="53">
+		</div>
+		<div align="center">
+ <b>Ящики <br>
+			 пищевые</b>
+		</div>
+ </li>
+	</ul>
+	<hr>
+	<div flex-align="center" flex-text_align="space-around">
+		<div class="col-lg-12 col-md-12 col-xs-24" align="center">
+			<div class="col-lg-21 col-md-21 colxs-24">
+ <img src="https://plastic-system.ru/arenda-puling/KLTcontainers.jpg">
 			</div>
-			<hr>
-			<ul class="inline_list" flex-align="end">
-				<li>
-				<div class="inline_list-img">
-		 <img width="72" src="/upload/content/about-company/catalog_section-1.svg" height="56">
-				</div>
-				<div>
-		 <b>Поликарбонат <br>
-					 листовой</b>
-				</div>
-		 </li>
-				<li>
-				<div class="inline_list-img">
-		 <img width="82" src="/upload/content/about-company/catalog_section-2.svg" height="60">
-				</div>
-				<div>
-		 <b>Полипропилен <br>
-					 листовой</b>
-				</div>
-		 </li>
-				<li>
-				<div class="inline_list-img">
-		 <img width="80" src="/upload/content/about-company/catalog_section-3.svg" height="45">
-				</div>
-				<div>
-		 <b>Пластиковые <br>
-					 паллеты</b>
-				</div>
-		 </li>
-				<li>
-				<div class="inline_list-img">
-		 <img width="52" src="/upload/content/about-company/catalog_section-4.svg" height="66">
-				</div>
-				<div>
-		 <b>Модульные <br>
-					 стеллажи</b>
-				</div>
-		 </li>
-				<li>
-				<div class="inline_list-img">
-		 <img width="80" src="/upload/content/about-company/catalog_section-5.svg" height="49">
-				</div>
-				<div>
-		 <b>Складские<br>
-					 лотки</b>
-				</div>
-		 </li>
-				<li>
-				<div class="inline_list-img">
-		 <img width="73" src="/upload/content/about-company/catalog_section-6.svg" height="53">
-				</div>
-				<div>
-		 <b>Пластиковые <br>
-					 контейнеры</b>
-				</div>
-		 </li>
-			</ul>
-			<hr>
+			<h3>
+				Пластиковые контейнеры KLT
+			</h3>
+		</div>
+		<div class="col-lg-12 col-md-12 col-xs-24" align="center">
+			<div class="col-lg-21 col-md-29 colxs-24">
+ <img src="https://plastic-system.ru/arenda-puling/evrocontainers.jpg">
+			</div>
+			<h3>
+				  Пластиковые Евроконтейнеры ЕС
+			</h3>
+		</div>
+	</div>
+	<hr>
 			<div flex-align="start">
-				<div class="content col-lg-12">
+				<div align="center">
 					<p>
-						 Вы будете довольны сотрудничеством с нашей компанией, ведь мы не только предоставляем качественную продукцию по минимальной цене, а имеем комплексный подход к каждому клиенту, начиная от помощи в подборе тары или упаковки в зависимости от условий эксплуатации, возможности нанесения логотипа компании, а также реализацию нестандартных проектов.
+						Выполнены в соответствии с требованям и европейского стандарта VDA 4500 (Объединение автомобильной промышленности Германии). Обладают высокой прочностью и износостойкостью. Размеры адаптированы для работы на автоматизированных линиях. Максимально подходят для автосборочных производств и машиностроения.
 					</p>
 				</div>
-				<div class="content col-lg-12">
+			</div>
+				<hr>
+	<div flex-align="center" flex-text_align="space-around">
+		<div class="col-lg-12 col-md-12 col-xs-24" align="center">
+			<div class="col-lg-21 col-md-21 colxs-24">
+ <img src="https://plastic-system.ru/arenda-puling/bigbox.jpg">
+			</div>
+			<h3>
+				 Крупногабаритные контейнеры
+			</h3>
+		</div>
+		<div class="col-lg-12 col-md-12 col-xs-24" align="center">
+			<div class="col-lg-21 col-md-29 colxs-24">
+ <img src="https://plastic-system.ru/arenda-puling/skladnyekrupnogabarit.jpg">
+			</div>
+			<h3>
+				 Складные крупногабаритные контейнеры
+			</h3>
+		</div>
+	</div>
+	<hr>
+			<div flex-align="start">
+				<div align="center">
 					<p>
-						 Вы можете отправить заявку на покупку любого из представленных продуктов в нашем сайте&nbsp;или обратиться за консультацией в отдел продаж по телефону <b>+7</b><b>(812)313-75-50</b>.
+						Все арендуемые крупногабаритные контейнеры могут быть подобраны как для пищевой промышленности, с учетом всех технических особенностей тары, так и для непищевой промышленной транспортировки. Все особенности тары согласуются с клиентом предварительно.
 					</p>
+				</div>
+			</div>
+				<hr>
+	<div flex-align="center" flex-text_align="space-around">
+		<div class=align="center">
+ <img src="https://plastic-system.ru/arenda-puling/pallety1.jpg">
+			</div>
+			<h3>
+				 Пластиковые паллеты
+			</h3>
+		</div>
+	<hr>
+			<div flex-align="start">
+				<div align="center">
+					<p>
+						Выполнены в соответствии с требованям и европейского стандарта VDA 4500 (Объединение автомобильной промышленности Германии). Обладают высокой прочностью и износостойкостью. Размеры адаптированы для работы на автоматизированных линиях. Максимально подходят для автосборочных производств и машиностроения.
+					</p>
+				</div>
+			</div>
+				<hr>
+	<div flex-align="center" flex-text_align="space-around">
+		<div class=align="center">
+ <img src="https://plastic-system.ru/arenda-puling/skladskyelotki.jpg">
+			</div>
+			<h3>
+				 Складские лотки Logic Store и SK
+			</h3>
+		</div>
+	<hr>
+			<div flex-align="start">
+				<div align="center">
+					<p>
+						Выполнены в соответствии с требованям и европейского стандарта VDA 4500 (Объединение автомобильной промышленности Германии). Обладают высокой прочностью и износостойкостью. Размеры адаптированы для работы на автоматизированных линиях. Максимально подходят для автосборочных производств и машиностроения.
+					</p>
+				</div>
+			</div>
+				<hr>
+	<div flex-align="center" flex-text_align="space-around">
+		<div class=align="center">
+ <img src="https://plastic-system.ru/arenda-puling/pishevye.jpg">
+			</div>
+			<h3>
+				 Пищевые многооборотные ящики
+			</h3>
+		</div>
+	<hr>
+			<div flex-align="start">
+				<div align="center">
+					<p>
+						Выполнены в соответствии с требованям и европейского стандарта VDA 4500 (Объединение автомобильной промышленности Германии). Обладают высокой прочностью и износостойкостью. Размеры адаптированы для работы на автоматизированных линиях. Максимально подходят для автосборочных производств и машиностроения.
+					</p>
+				</div>
+			</div>
+				<hr>
+			<div flex-align="start">
+				<div align="center">
+					<b>
+						 Вы будете довольны сотрудничеством с нашей компанией, ведь мы не только предоставляем качественную продукцию по минимальной цене, а имеем комплексный подход к каждому клиенту, начиная от помощи в подборе тары или упаковки в зависимости от условий эксплуатации, возможности нанесения логотипа компании, а также реализацию нестандартных проектов.
+					</b>
+				</div>
+				<hr>
+				<div align="center">
+					<b>
+						 Вы можете отправить заявку на покупку любого из представленных продуктов в нашем сайте&nbsp;или обратиться за консультацией в отдел продаж по телефону <b>+7</b><b>(812)313-75-50</b>.
+					</b>
 				</div>
 			</div>
 		</div>
- <br><?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
+<?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
